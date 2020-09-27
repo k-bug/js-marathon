be610aaed4f6fc5dd351fa99814e6d061d600ebd
Kirill Bugaev <bugaev.kirill@gmail.com>
2020-09-27 13:59:44 +0300

    Homework-3

--- main.js
+++ main.js
@@ -1,16 +1,61 @@
-const firstRow = 'мама мыла раму';
-const secondRow = 'собака друг человека';
+const $btn = document.getElementById('btn-kick');
 
-function countLetters (string) {
-    let letterQ = 0;
-    for (let i = 0; i < string.length; i++) {
-        letterQ++
+const character = {
+    name: 'Pikachu',
+    defaultHP: 100,
+    damageHP: 100,
+    elHP: document.getElementById('health-character'),
+    elProgressbar: document.getElementById('progressbar-character'),
+}
+
+const enemy = {
+    name: 'Charmander',
+    defaultHP: 100,
+    damageHP: 100,
+    elHP: document.getElementById('health-enemy'),
+    elProgressbar: document.getElementById('progressbar-enemy'),
+}
+
+$btn.addEventListener('click', function () {
+    console.log('Kick')
+    changeHP(randome(20), character);
+    changeHP( randome(20), enemy);
+})
+
+function init() {
+    console.log('Start Game!');
+    renderHP(character);
+    renderHP(enemy);
+}
+
+function renderHP (person) {
+    renderHPLife(person);
+    renderProgressbarHP(person);
+}
+
+function renderHPLife(person) {
+    person.elHP.innerText = person.damageHP + ' / ' + person.defaultHP;
+}
+
+function renderProgressbarHP (person) {
+    person.elProgressbar.style.width = person.damageHP + '%';
+}
+
+function changeHP (count, person) {
+    if (person.damageHP < count) {
+        person.damageHP;
+
+        alert('Бедный ' + person.name + ' проиграл бой!');
+        $btn.disabled = true;
+    } else {
+        person.damageHP -= count;
     }
-    return letterQ;
+
+    renderHP(person);
 }
 
-function getRow(firstRow, secondRow) {
-    return countLetters(firstRow) > countLetters(secondRow) ? firstRow : secondRow;
+function randome (num) {
+    return Math.ceil(Math.random() * num);
 }
 
-console.log(getRow(firstRow, secondRow));
+init();
